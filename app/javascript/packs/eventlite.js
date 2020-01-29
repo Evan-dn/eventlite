787a11dcd1022e3e5348cd59c2436ca4c0c97a47
evan wichlarz <evanwichlarz1@hotmail.fr>
2020-01-29 10:40:41 -0500

    creation des composants react avec props et rendue de index.erb en react

--- app/javascript/packs/eventlite.js
+++ app/javascript/packs/eventlite.js
@@ -1,18 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import EventsList from './eventslist'
 
 const EventLite = props => (
     <div>
         <h1>Eventlite</h1>
-        {props.events.map(function(e){
-            return(
-                <div>
-                    <h2>Title : {e.title}</h2>
-                    <p>Location : {e.location}</p>
-                    <p>Date : {e.datetime}</p>
-                </div>
-            )
-        })}
+        <EventsList events={props.events}/>
     </div>
 )
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  get 'hello_world', to: 'hello_world#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'events#index'
-  resources :events, only: [:index]
+  resources :events
 end
