78f5ef84e1965533b9558d949924515895e69df3
evan wichlarz <evanwichlarz1@hotmail.fr>
2020-01-29 06:59:35 -0500

    creation seed et de la vue index pour voir les events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,43 @@
 class EventsController < ApplicationController
+
+    before_action :set_event, only: [:show, :update, :edit, :destroy]
+
+    def index
+        @events = Event.all
+    end
+
+    def show
+    
+    end
+
+    def new
+        @event = Event.new
+    end
+
+    def create
+        @event = Event.new(event_params)
+        @event.save
+    end
+
+    def edit
+    end
+
+    def update 
+        @event.update(event_params)
+    end
+
+    def destroy
+        @event.destroy
+    end
+
+    private
+
+    def set_event
+        @event = Event.find(params[:id])
+    end
+
+    def event_params
+        params.require(:event).permit(:title, :location, :datetime)
+    end
+    
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+puts "cleaning DB"
+Event.destroy_all
+
+puts "create events"
+
+event_attributes = [
+    {
+        title: "london retail expo",
+        datetime: "Monday 21 Oct, 2019 10:00 AM GMT",
+        location: "London Excel Centre"
+    },
+    {
+        title: "Entreprise Sales Training Worshop",
+        datetime: "Tuesday 22 Oct, 2019 10:00 AM GMT",
+        location: "Expert sales Company Headquarters"
+    },
+    {
+        title: "Ruby Hack Night",
+        datetime: "Friday 25 Oct, 2019 06:00 PM GMT",
+        location: "Learnetto Headquarters"
+    },
+    {
+        title: "Salsa Meetup",
+        datetime: "Saturday 26 Oct, 2019 06:00 PM GMT",
+    }
+]
+
+Event.create(event_attributes)
+puts "finished"
